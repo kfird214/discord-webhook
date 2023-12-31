806cb70f8a46584e93b8d213f6db865433cf9510
kfir <kfird214@gmail.com>
2023-12-31 02:17:56 +0200

    console.debug process env jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import { config as dotconfig } from 'dotenv';
 
 dotconfig();
 
+console.debug('jest.config.ts: process.env: \n  ' + JSON.stringify(process.env, null, 2).split('\n').join('\n    '));
+
 const config: Config = {
     verbose: true,
     preset: 'ts-jest',
